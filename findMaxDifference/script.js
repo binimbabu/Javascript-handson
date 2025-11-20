f506b7ff7835f51df013e1971421ceb9c131b2bc
Bini Babu <binibabu22@gmail.com>
2025-11-20 20:42:57 +0530

    Find maximum difference between elements in an array

---
+++ findMaxDifference/script.js
@@ -0,0 +1,18 @@
+//find Maximum Difference between elements in an array
+
+function findMaxDifference(arr) {
+  if (arr.length < 2) return 0;
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max - min;
+}
+
+console.log(findMaxDifference([10, 20, 30, 40]));
